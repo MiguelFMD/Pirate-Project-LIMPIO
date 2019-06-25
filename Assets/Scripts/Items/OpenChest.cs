0efc0bc7159388c6a88afdff4e307ff5656b25e2
Álvaro Martínez Soto <halvaren@users.noreply.github.com>
2019-06-25 18:29:28 +0200

    Arreglos finales

--- Assets/Scripts/Items/OpenChest.cs
+++ Assets/Scripts/Items/OpenChest.cs
@@ -8,41 +8,19 @@ using Cinemachine;
 
 public class OpenChest : MonoBehaviour
 {
-    //public GameObject player; 
-    // Start is called before the first frame update
     public Camera mainCam;
     public CinemachineVirtualCamera dollyCam;
-    //public SceneController sController;
-    private bool nearChest;
-    private bool keyPressed;
-    void Start()
-    {
-        nearChest = false;
-        keyPressed = false; 
-    }
 
     private IEnumerator openingAnimation ()
     {
         GetComponent<Animator>().SetBool("isOpening", true);
-        Debug.Log("Function Run");
         yield return new WaitForSeconds (5.0f);
-        // sController.GetComponent<SceneController>().StartScoreScreen();
         GameManager.Instance.SceneController.StartScoreScreen();
-      
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        if (nearChest) {
-            if(Input.GetKeyDown(KeyCode.Z)) keyPressed = true;
-        }
     }
     
-    private void OnTriggerEnter (Collider other) {
-        if ((other.gameObject.tag == "Player")) {
-           //  Debug.Log("ey");
-            //nearChest = true; 
+    private void OnTriggerStay (Collider other) {
+
+        if (other.gameObject.tag == "Player" && other.GetComponent<PlayerHealthController>().HasKey()) {
             if(Input.GetKeyDown(KeyCode.Z))
             {
                 GameManager.Instance.LocalPlayer.playerOff = true; 
